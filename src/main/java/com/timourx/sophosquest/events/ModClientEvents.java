5507352708335480f663354bd3205dd63d5540a0
TimourX <timouryildiz2004@gmail.com>
2020-09-01 20:43:10 +0200

    Moofah Fur Armor is now dyeable.

--- src/main/java/com/timourx/sophosquest/events/ModClientEvents.java
+++ src/main/java/com/timourx/sophosquest/events/ModClientEvents.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.ArrayList;
 
 @Mod.EventBusSubscriber(modid = SophosQuest.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
-public class ForgeBusClientEvents {
+public class ModClientEvents {
 
     @SubscribeEvent
     public static void onClientPlayerAttackEntity(AttackEntityEvent event) {
--- src/main/java/com/timourx/sophosquest/events/ModServerEvents.java
+++ src/main/java/com/timourx/sophosquest/events/ModServerEvents.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.ArrayList;
 
 @Mod.EventBusSubscriber(modid = SophosQuest.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.DEDICATED_SERVER)
-public class ForgeBusServerEvents {
+public class ModServerEvents {
 
     @SubscribeEvent
     public static void onServerPlayerAttackEntity(AttackEntityEvent event) {
