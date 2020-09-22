ce867fe3ab28491e3bc9269f0184348731341504
TimourX <timouryildiz2004@gmail.com>
2020-09-22 20:19:16 +0200

    Petrified Blade improved.

--- src/main/java/com/timourx/sophosquest/items/PetrifiedBladeItem.java
+++ src/main/java/com/timourx/sophosquest/items/PetrifiedBladeItem.java
@@ -1,20 +1,36 @@
 package com.timourx.sophosquest.items;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 
-import javax.annotation.Nonnull;
+import java.util.UUID;
 
 public class PetrifiedBladeItem extends SwordItem {
 
+    private final Multimap<Attribute, AttributeModifier> attributeModifiers;
 
     public PetrifiedBladeItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builderIn) {
         super(tier, attackDamageIn, attackSpeedIn, builderIn);
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        builder.putAll(super.getAttributeModifiers(EquipmentSlotType.MAINHAND));
+        builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(UUID.fromString("1bb4741d-77ed-4cd4-b5a2-34a49b680acb"), "Slowness caused by heavy object", -0.55d, AttributeModifier.Operation.MULTIPLY_TOTAL));
+        this.attributeModifiers = builder.build();
     }
 
     @Override
-    public boolean isEnchantable(@Nonnull ItemStack stack) {
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+        return equipmentSlot == EquipmentSlotType.MAINHAND ? this.attributeModifiers : super.getAttributeModifiers(equipmentSlot);
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
         return false;
     }
 }
